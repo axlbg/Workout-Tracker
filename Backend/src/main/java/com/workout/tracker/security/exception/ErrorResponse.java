57e8b6bfc8cb469ea825dbb9cf1988b011bb8419
axlbg <axl_bg@hotmail.com>
2025-02-19 21:10:25 -0300

    lombok errorresponse

--- Backend/src/main/java/com/workout/tracker/security/exception/ErrorResponse.java
+++ Backend/src/main/java/com/workout/tracker/security/exception/ErrorResponse.java
@@ -1,17 +1,14 @@
 package com.workout.tracker.security.exception;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ErrorResponse {
     private String error;
 
     public ErrorResponse(String error) {
-        this.error = error;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
         this.error = error;
     }
 }
